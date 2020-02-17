bad3e6d53359bf65692072b797ad5a0b9c640cb0
Marcelo Augusto Rauh Schmitt <marcelo.schmitt@poa.ifrs.edu.br>
2020-02-17 13:59:04 -0300

    Codechecker

--- classes/provider.php
+++ classes/provider.php
@@ -14,8 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-require('../../../config.php');
 namespace block_analytics_graphs\privacy;
+defined('MOODLE_INTERNAL') || die();
 
 class provider implements
     // This plugin does not store any personal user data.
